9756c9cf396b97fd026e9ea3e9d09c6cf0446baa
Robin Raju <robin.raju@petrolink.com>
2020-01-22 07:29:52 +0100

    Modified dev scripts

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -82,4 +82,5 @@ $(document).ready(function() {
     closeOnContentClick: true,
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
+  throw Error("Remember: View source is cheating.  haha.. just kidding :)");
 });
